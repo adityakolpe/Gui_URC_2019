da7ac7ba1189a66440e5becb0dca54a914b4cd68
adityakolpe <adityakolpe19@gmail.com>
2019-02-04 22:40:17 +0530

    04/02/2019

--- cam10.py
+++ cam10.py
@@ -8,23 +8,12 @@ while(True):
     #ret1, frame1 = cap1.read()
     frame=cv2.resize(frame,(480,360))
     # Display the resulting frame
-    cv2.imshow('cam10',frame)
+    cv2.imshow('cam9',frame)
     #cv2.imshow('frame1',frame1)
-cap = cv2.VideoCapture('rtsp://192.168.1.10/user=admin&password=&channel=2&stream=0.sdp')
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    ret1, frame1 = cap1.read()
-
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
-    cv2.imshow('frame1',frame1)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
 cap.release()
 #cap1.release()
-cv2.destroyAllWindows()
-cap1.release()
 cv2.destroyAllWindows()
--- cam9.py
+++ cam9.py
@@ -10,21 +10,10 @@ while(True):
     # Display the resulting frame
     cv2.imshow('cam9',frame)
     #cv2.imshow('frame1',frame1)
-cap = cv2.VideoCapture('rtsp://192.168.1.9/user=admin&password=&channel=2&stream=0.sdp')
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    ret1, frame1 = cap1.read()
-
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
-    cv2.imshow('frame1',frame1)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
 cap.release()
 #cap1.release()
-cv2.destroyAllWindows()
-cap1.release()
 cv2.destroyAllWindows()
